f2aecfbb5d10be5d4cf07c36cb27c4e5898f248f
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-08-20 13:29:10 -0500

    feeling component redux functionality working.

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,14 +1,30 @@
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 function Feeling () {
 
     const history = useHistory();
+    const dispatch = useDispatch()
 
+    let [feedback, setFeedback] = useState({feeling: '', understanding: '', support: '', comments:''})
+
+    const handleInput = (event) =>{
+        setFeedback({
+            ...feedback, 
+            feeling: event.target.value});
+    }
     const handleNext = () => {
+        dispatch({
+            type: 'SET_FEELING',
+            payload: feedback
+        })
+
         history.push('/understanding');
 
     }//end of handleNext
 
+    console.log(feedback);
     return (
         <>
             <h2>How are you feeling today?</h2>
@@ -18,6 +34,7 @@ function Feeling () {
                 id="feeling"
                 min="0" 
                 max="10"
+                onChange={handleInput}
             
             />
             <button
