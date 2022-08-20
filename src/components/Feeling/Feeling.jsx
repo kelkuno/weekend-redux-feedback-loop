99c62e338f3cce24965fb404aa1bb8f217c607ca
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-08-20 09:36:50 -0500

    feeling html

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,7 +1,17 @@
 function Feeling () {
 
     return (
-        <p>feeling is here</p>
+        <>
+            <h2>How are you feeling today?</h2>
+            <label>Feeling?</label>
+            <input
+                type="number"
+                id="feeling"
+                min="0" 
+                max="10"
+            
+            />
+        </>
     )
 
 }//end of Feeling
